f139087d73687e0ba0d9d362c352b92ca1d87694
githubartist6 <githubartist@gmail.com>
2024-06-08 09:37:15 +0530

    computer class

---
+++ computer.js
@@ -0,0 +1,11 @@
+let computer = document.getElementById("computer");
+let computer11 = document.getElementById("computer1");
+let computer22 = document.getElementById("computer2");
+
+computer11.addEventListener("click", function () {
+    computer.style.backgroundImage="url(class1.jpg)"
+});
+
+computer22.addEventListener("click", function () {
+    computer.style.backgroundImage="url(sir.png)"
+});
